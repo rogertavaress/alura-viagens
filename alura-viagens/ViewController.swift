213c1f85c8b4e0752a5022d975ad8a92ae2ea8f9
Rogério Tavares <rogerioctf@gmail.com>
2021-12-18 15:30:35 -0300

    first commit

---
+++ alura-viagens/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  alura-viagens
+//
+//  Created by Rogério Tavares on 18/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var viagensTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viagensTableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "Viagem \(indexPath.row)"
+        
+        return cell
+    }
+}
