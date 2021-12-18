ae7270878d5a6da8594c48341f9c3cfd44f3382e
Rogério Tavares <rogerioctf@gmail.com>
2021-12-18 17:47:51 -0300

    criando header

---
+++ alura-viagens/HomeTableViewHeader.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTableViewHeader.swift
+//  alura-viagens
+//
+//  Created by Rogério Tavares on 18/12/21.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var bannerImageView: UIImageView!
+}
--- alura-viagens/ViewController.swift
+++ alura-viagens/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viagensTableView.dataSource = self
+        viagensTableView.delegate = self
+        view.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
     }
 }
 
@@ -28,5 +30,17 @@ extension ViewController: UITableViewDataSource {
         cell.textLabel?.text = "Viagem \(indexPath.row)"
         
         return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self, options: nil)?.first as? HomeTableViewHeader
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
     }
 }
