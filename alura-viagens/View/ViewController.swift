414b8f4cffceaf97b659c58f81ff32185dd87582
Rogério Tavares <rogerioctf@gmail.com>
2021-12-20 21:32:52 -0300

    size classes

--- alura-viagens/View/ViewController.swift
+++ alura-viagens/View/ViewController.swift
@@ -60,6 +60,6 @@ extension ViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 400
+        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 400 : 475
     }
 }
